535a26bbc222fa020a9b9d9b9670ce2710d3c367
LANDHIGHTECH101\pgu <pgu@landhightech.com>
2022-12-01 17:06:53 +0800

    build: insert data into db file

--- Views/Item/Book/view.js
+++ Views/Item/Book/view.js
@@ -1,3 +1,17 @@
 /**
  * Custom render view for book
  */
+const {
+    book: {
+        Name: name,
+        Content: content
+    }
+} = input;
+const admonition = `
+\`\`\`ad-book
+title: ${name}
+collapse: close
+${content}
+\`\`\`
+`;
+dv.paragraph(admonition);
