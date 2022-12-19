a1d6d0f13b3c363e6654af41ac08faaf5aa7a1c9
LANDHIGHTECH101\pgu <pgu@landhightech.com>
2022-12-19 17:49:16 +0800

    build: optimize display style of enemy db files

--- Views/Enemy/Boss/view.js
+++ Views/Enemy/Boss/view.js
@@ -38,7 +38,7 @@ ${SepithLootTable(Sepiths) /* 掉落晶片 */}
 </tr>
 <tr>
 <td>
-${ItemLootTable(Items||[]) /* 掉落物品 */}
+${ItemLootTable(Items || []) /* 掉落物品 */}
 </td>
 </tr>
 <tr>
@@ -74,7 +74,7 @@ const PropertyTable = (id, properties, desc) => {
 <table class="description-table">
 <tbody>
 <tr>
-<td><img class="enemy-${id}-image"></td>
+<td><img class="enemy-${id}-image icon-min-size-monster"></td>
 <td>
 <table class="property-table">
 <tbody>
@@ -141,26 +141,48 @@ const ArtRateTable = (rates) => {
     </thead>
     <tbody>
         <tr>
-            <td><img class="icon-element-earth"></td>
-            <td>${earthArtRate}</td>
-            <td><img class="icon-element-time"></td>
-            <td>${timeArtRate}</td>
+            <td class="icon-td"><img class="icon-element-earth icon-min-size"></td>
+            <td><div style="width: ${
+              (earthArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-earth">
+            </div>
+            <span>${earthArtRate}</span></td>
+            <td class="icon-td"><img class="icon-element-time icon-min-size"></td>
+            <td><div style="width: ${
+              (timeArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-time"></div>
+            <span>${timeArtRate}</span></td>
         </tr>
         <tr>
-            <td><img class="icon-element-water"></td>
-            <td>${waterArtRate}</td>
-            <td><img class="icon-element-space"></td>
-            <td>${spaceArtRate}</td>
+            <td class="icon-td"><img class="icon-element-water icon-min-size"></td>
+            <td><div style="width: ${
+              (waterArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-water"></div>
+            <span>${waterArtRate}</span></td>
+            <td class="icon-td"><img class="icon-element-space icon-min-size"></td>
+            <td><div style="width: ${
+              (spaceArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-space"></div>
+            <span>${spaceArtRate}</span></td>
         </tr>
         <tr>
-            <td><img class="icon-element-fire"></td>
-            <td>${fireArtRate}</td>
-            <td><img class="icon-element-mirage"></td>
-            <td>${mirageArtRate}</td>
+            <td class="icon-td"><img class="icon-element-fire icon-min-size"></td>
+            <td><div style="width: ${
+              (fireArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-fire"></div>
+            <span>${fireArtRate}</span></td>
+            <td class="icon-td"><img class="icon-element-mirage icon-min-size"></td>
+            <td><div style="width: ${
+              (mirageArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-mirage"></div>
+            <span>${mirageArtRate}</span></td>
         </tr>
         <tr>
-            <td><img class="icon-element-wind"></td>
-            <td>${windArtRate}</td>
+            <td class="icon-td"><img class="icon-element-wind icon-min-size"></td>
+            <td><div style="width: ${
+              (windArtRate / 200) * 100
+            }%;" class="element-rate-bar element-rate-bar-wind"></div>
+            <span>${windArtRate}</span></td>
         </tr>
     </tbody>
 </table>
@@ -187,23 +209,23 @@ const SepithLootTable = (loots) => {
     </thead>
     <tbody>
         <tr>
-            <td><img class="icon-sepith-earth" /></td>
+            <td><img class="icon-sepith-earth icon-min-size-sepith" /></td>
             <td>${earthSepithLoot}</td>
-            <td><img class="icon-sepith-water" /></td>
+            <td><img class="icon-sepith-water icon-min-size-sepith" /></td>
             <td>${waterSepithLoot}</td>
-            <td><img class="icon-sepith-fire" /></td>
+            <td><img class="icon-sepith-fire icon-min-size-sepith" /></td>
             <td>${fireSepithLoot}</td>
-            <td><img class="icon-sepith-wind" /></td>
+            <td><img class="icon-sepith-wind icon-min-size-sepith" /></td>
             <td>${windSepithLoot}</td>
         </tr>
         <tr>
-            <td><img class="icon-sepith-time" /></td>
+            <td><img class="icon-sepith-time icon-min-size-sepith" /></td>
             <td>${timeSepithLoot}</td>
-            <td><img class="icon-sepith-space" /></td>
+            <td><img class="icon-sepith-space icon-min-size-sepith" /></td>
             <td>${spaceSepithLoot}</td>
-            <td><img class="icon-sepith-mirage" /></td>
+            <td><img class="icon-sepith-mirage icon-min-size-sepith" /></td>
             <td>${mirageSepithLoot}</td>
-            <td><img class="icon-sepith-mass" /></td>
+            <td><img class="icon-sepith-mass icon-min-size-sepith" /></td>
             <td>${massSepithLoot}</td>
         </tr>
     </tbody>
@@ -226,7 +248,7 @@ const ItemLootTable = (loots) => {
     }" href="${fileName[fileName.length - 1]}#^${
       link.subpath
     }" class="internal-link data-link-icon data-link-icon-after data-link-text" target="_blank" rel="noopener" data-link-id="${
-      file.ID
+      file.ID || ""
     }" data-link-tags="" data-link-path="${link.path}">${
       link.display || ""
     }</a>`;
@@ -322,57 +344,57 @@ const StatusRateTable = (status) => {
     </thead>
     <tbody>
         <tr>
-            <td><img class="status-poison" /></td>
+            <td class="icon-td"><img class="status-poison icon-min-size" /></td>
             <td>中毒</td>
             <td>${poisonRate}</td>
-            <td><img class="status-burn" /></td>
+            <td class="icon-td"><img class="status-burn icon-min-size" /></td>
             <td>炎伤</td>
             <td>${burnRate}</td>
-            <td><img class="status-death" /></td>
+            <td class="icon-td"><img class="status-death icon-min-size" /></td>
             <td>即死</td>
             <td>${deathRate}</td>
         </tr>
         <tr>
-            <td><img class="status-seal" /></td>
+            <td class="icon-td"><img class="status-seal icon-min-size" /></td>
             <td>封技</td>
             <td>${sealRate}</td>
-            <td><img class="status-freeze" /></td>
+            <td class="icon-td"><img class="status-freeze icon-min-size" /></td>
             <td>冻结</td>
             <td>${freezeRate}</td>
-            <td><img class="status-nightmare" /></td>
+            <td class="icon-td"><img class="status-nightmare icon-min-size" /></td>
             <td>恶梦</td>
             <td>${nightmareRate}</td>
         </tr>
         <tr>
-            <td><img class="status-mute" /></td>
+            <td class="icon-td"><img class="status-mute icon-min-size" /></td>
             <td>封魔</td>
             <td>${muteRate}</td>
-            <td><img class="status-petrify" /></td>
+            <td class="icon-td"><img class="status-petrify icon-min-size" /></td>
             <td>石化</td>
             <td>${petrifyRate}</td>
-            <td><img class="status-delay" /></td>
+            <td class="icon-td"><img class="status-delay icon-min-size" /></td>
             <td>延迟</td>
             <td>${delayRate}</td>
         </tr>
         <tr>
-            <td><img class="status-blind" /></td>
+            <td class="icon-td"><img class="status-blind icon-min-size" /></td>
             <td>黑暗</td>
             <td>${blindRate}</td>
-            <td><img class="status-faint" /></td>
+            <td class="icon-td"><img class="status-faint icon-min-size" /></td>
             <td>气绝</td>
             <td>${faintRate}</td>
-            <td><img class="status-vanish" /></td>
+            <td class="icon-td"><img class="status-vanish icon-min-size" /></td>
             <td>消失</td>
             <td>${vanishRate}</td>
         </tr>
         <tr>
-            <td><img class="status-sleep" /></td>
+            <td class="icon-td"><img class="status-sleep icon-min-size" /></td>
             <td>睡眠</td>
             <td>${sleepRate}</td>
-            <td><img class="status-confuse" /></td>
+            <td class="icon-td"><img class="status-confuse icon-min-size" /></td>
             <td>混乱</td>
             <td>${confuseRate}</td>
-            <td><img class="status-s-down" /></td>
+            <td class="icon-td"><img class="status-s-down icon-min-size" /></td>
             <td>能力降低</td>
             <td>${sDownRate}</td>
         </tr>
